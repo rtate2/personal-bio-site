2f10cc1f064eb07301f42cb09f58c4f47aea1bd9
Randy Tate <randytatejr@gmail.com>
2019-09-14 14:28:54 -0500

    added boilerplate

--- projects.js
+++ projects.js
@@ -17,35 +17,67 @@ const projects = [
         available: true,
         url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
         githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+    },
+
+    {
+        title: "Cool Project", 
+        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: true,
+        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+    },
+
+    {
+        title: "Cool Project", 
+        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: false,
+        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+    },
+
+    {
+        title: "Cool Project", 
+        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: true,
+        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
     }
-]
+];
 
 // create a printToDom function - takes two parameters
 // DivID and textToPrint
 const printToDom = (divID, textToPrint) => {
-    containerID = document.getElementById(divID);
-    containerID.innerHTML = textToPrint;
+    const containerId = document.getElementById(divID);
+    containerId.innerHTML = textToPrint;
 };
 
 // create second function
 const createProjectCards = (arr) => {
     let domString = '';
     for (let i = 0; i < arr.length; i++) {
+        const projectArr = arr[i]
+        if (projectArr.available){
+            console.log(projectArr.available)
         // question concerning the div id tag to line 26
         domString += `
          <div class='projectCard'>
-            <h2>${arr[i].title}</h2>
-            <img src=${arr[i].screenshot}></img>
-            <p>${arr[i].description}</p>
-            <p>${arr[i].technologiesUsed}</p>
-            <p>${arr[i].available}</p>
-            <img src=${arr[i].url}></img>
-            <img src=${arr[i].githubUrl}></img>
+            <h2>${projectArr.title}</h2>
+            <img src=${projectArr.screenshot}></img>
+            <p>${projectArr.description}</p>
+            <p>${projectArr.technologiesUsed}</p>
+            <p>${projectArr.available}</p>
+            <img src=${projectArr.url}></img>
+            <img src=${projectArr.githubUrl}></img>
         </div>
-        `
+        `;
+        }
     }
-
-    // calling printToDom
     printToDom('projectsPage', domString)
 };
 
